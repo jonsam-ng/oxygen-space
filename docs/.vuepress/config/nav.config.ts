e5973e8c3beca9834ca2dc837e4aca90b266d988
wuqingshan <wuqingshan@xylink.com>
2022-07-21 09:16:19 +0800

    chore: 🛠  update site configs

--- docs/.vuepress/config/nav.config.ts
+++ docs/.vuepress/config/nav.config.ts
@@ -23,6 +23,7 @@ export default [
       { text: "关于", link: "/about/" },
       // generate by slidev
       { text: "幻灯片", link: `/slides/#/1` },
+      { text: "导航", link: `https://source.jonsam.site/nav/` },
       { text: "鸠摩搜书", link: `https://www.jiumodiary.com/` },
       { text: "微信读书", link: `https://weread.qq.com/` },
     ],
